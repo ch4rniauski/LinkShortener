d91ff2320a80b2d6386d7fd2a93fd34e7d83d367
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-02-22 20:53:59 +0300

    Add angular

---
+++ frontend/src/app/common-ui/link-shortener-form/link-shortener-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-link-shortener-form',
+  imports: [],
+  templateUrl: './link-shortener-form.html',
+  styleUrl: './link-shortener-form.scss',
+})
+export class LinkShortenerForm {
+
+}

b983707e280a9e6529b495eff5f351d9cd40d736
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-02-25 08:11:44 +0300

    Add DeleteShortLinkUnitTests

---
+++ LinkShortener.Tests/Tests/UnitTests/ShortLink/DeleteShortLinkUnitTests.cs
@@ -0,0 +1,112 @@
+using ch4rniauski.LinkShortener.Application.Common.Errors;
+using ch4rniauski.LinkShortener.Application.Common.Results;
+using ch4rniauski.LinkShortener.Application.Contracts.Repositories;
+using ch4rniauski.LinkShortener.Application.Dto.ShortLink.Responses;
+using ch4rniauski.LinkShortener.Application.UseCases.CommandHandlers.ShortLink;
+using ch4rniauski.LinkShortener.Application.UseCases.Commands.ShortLink;
+using ch4rniauski.LinkShortener.Domain.Entities;
+using Moq;
+using Xunit;
+
+namespace LinkShortener.Tests.Tests.UnitTests.ShortLink;
+
+public sealed class DeleteShortLinkUnitTests
+{
+    private readonly DeleteShortLinkCommandHandler _commandHandler;
+    private readonly Mock<IShortLinkRepository> _mockShortLinkRepository = new();
+
+    public DeleteShortLinkUnitTests()
+    {
+        _commandHandler = new DeleteShortLinkCommandHandler(_mockShortLinkRepository.Object);
+    }
+
+    [Fact]
+    private async Task DeleteShortLink_ReturnsSuccessfulResult()
+    {
+        // Arrange
+        const bool isSuccess = true;
+        
+        var id = Guid.NewGuid();
+        var command = new DeleteShortLinkCommand(id);
+        var entity = new ShortLinkEntity();
+        var responseDto = new DeleteShortLinkResponseDto(id);
+        
+        _mockShortLinkRepository.Setup(r => r.GetByIdAsync(
+                id, 
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(entity);
+
+        _mockShortLinkRepository.Setup(r => r.DeleteAsync(
+                entity,
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(isSuccess);
+
+        // Act
+        var response = await _commandHandler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsType<Result<DeleteShortLinkResponseDto>>(response);
+        Assert.NotNull(response.Value);
+        Assert.IsType<DeleteShortLinkResponseDto>(response.Value);
+        Assert.Equal(isSuccess, response.IsSuccess);
+        Assert.Equal(responseDto, response.Value);
+        Assert.Null(response.Error);
+    }
+    
+    [Fact]
+    private async Task DeleteShortLink_ReturnsFailedResult_WithNotFoundError()
+    {
+        // Arrange
+        const bool isSuccess = false;
+        
+        var id = Guid.NewGuid();
+        var command = new DeleteShortLinkCommand(id);
+        ShortLinkEntity? entity = null;
+        
+        _mockShortLinkRepository.Setup(r => r.GetByIdAsync(
+                id, 
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(entity);
+
+        // Act
+        var response = await _commandHandler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsType<Result<DeleteShortLinkResponseDto>>(response);
+        Assert.Null(response.Value);
+        Assert.NotNull(response.Error);
+        Assert.Equal(isSuccess, response.IsSuccess);
+        Assert.IsType<NotFoundError>(response.Error);
+    }
+    
+    [Fact]
+    private async Task DeleteShortLink_ReturnsFailedResult_WithInternalError()
+    {
+        // Arrange
+        const bool isSuccess = false;
+        
+        var id = Guid.NewGuid();
+        var command = new DeleteShortLinkCommand(id);
+        var entity = new ShortLinkEntity();
+        
+        _mockShortLinkRepository.Setup(r => r.GetByIdAsync(
+                id, 
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(entity);
+
+        _mockShortLinkRepository.Setup(r => r.DeleteAsync(
+                entity,
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(isSuccess);
+
+        // Act
+        var response = await _commandHandler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsType<Result<DeleteShortLinkResponseDto>>(response);
+        Assert.Null(response.Value);
+        Assert.NotNull(response.Error);
+        Assert.Equal(isSuccess, response.IsSuccess);
+        Assert.IsType<InternalServerError>(response.Error);
+    }
+}
